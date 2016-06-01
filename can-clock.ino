7eebc436cf63ca7c2f63291dfd063a3c05b5408f
p1ne <nnalutin@gmail.com>
2016-06-01 18:23:25 +0300

    Massive refactor of message sending - bound to common timer now

--- can-clock.ino
+++ can-clock.ino
@@ -40,6 +40,7 @@ class CANMessage {
 public:
   uint32_t started;
   uint32_t delayed;
+  uint32_t repeated;
   uint32_t header;
   
   byte len;
@@ -49,10 +50,11 @@ public:
   {
   }
 
-  CANMessage(uint32_t _started, uint32_t _delayed, uint32_t _header, byte _len, byte _d0, byte _d1, byte _d2, byte _d3, byte _d4, byte _d5, byte _d6, byte _d7)
+  CANMessage(uint32_t _started, uint32_t _delayed, uint32_t _repeated, uint32_t _header, byte _len, byte _d0, byte _d1, byte _d2, byte _d3, byte _d4, byte _d5, byte _d6, byte _d7)
   {
     started = _started;
     delayed = _delayed;
+    repeated = _repeated;
     header = _header;
     len = _len;
     data[0] = _d0;
@@ -65,10 +67,11 @@ public:
     data[7] = _d7;
   }
 
-  void set(uint32_t _started, uint32_t _delayed, uint32_t _header, byte _len, byte _d0, byte _d1, byte _d2, byte _d3, byte _d4, byte _d5, byte _d6, byte _d7)
+  void set(uint32_t _started, uint32_t _delayed, uint32_t _repeated, uint32_t _header, byte _len, byte _d0, byte _d1, byte _d2, byte _d3, byte _d4, byte _d5, byte _d6, byte _d7)
   {
     started = _started;
     delayed = _delayed;
+    repeated = _repeated;
     header = _header;
     len = _len;
     data[0] = _d0;
@@ -86,6 +89,8 @@ public:
     Serial.print(started);
     Serial.print(" ");
     Serial.print(delayed);
+    Serial.print(" ");
+    Serial.print(repeated);
     Serial.print(" ");
     Serial.print(header);
     Serial.print(" ");
@@ -111,48 +116,48 @@ public:
 
 };
 
-CANMessage start[8];
-CANMessage cycle[4];
-CANMessage text[12];
+CANMessage start[START_COUNT];
+CANMessage cycle[MSG_COUNT];
+CANMessage text[TEXT_COUNT];
 
 void initStartMessages()
 {
-  start[0].set( 0, 100, 0x50c, 3, 0x0C, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 );
-  start[1].set( 0, 250, 0x3e8, 8, 0x00, 0x00, 0x29, 0x00, 0x00, 0x00, 0x00, 0x00 );
-  start[2].set( 0,  50, 0x3ef, 8, 0x32, 0x32, 0x32, 0x32, 0x03, 0x00, 0x00, 0x00 );  
-  start[3].set( 0,  50, 0x3f2, 8, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x60, 0x00, 0x00 );
-  start[4].set( 0, 100, 0x50c, 3, 0x01, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 );
-  start[5].set( 0,  50, 0x3f2, 8, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xF0, 0x00, 0x00 );
-  start[6].set( 0,  50, 0x3f1, 8, 0xF5, 0x90, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 );
-  start[7].set( 0, 100, 0x50c, 3, 0x01, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 );
+  start[0].set( 0, 100, 0, 0x50c, 3, 0x0C, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 );
+  start[1].set( 0, 250, 0, 0x3e8, 8, 0x00, 0x00, 0x29, 0x00, 0x00, 0x00, 0x00, 0x00 );
+  start[2].set( 0,  50, 0, 0x3ef, 8, 0x32, 0x32, 0x32, 0x32, 0x03, 0x00, 0x00, 0x00 );  
+  start[3].set( 0,  50, 0, 0x3f2, 8, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x60, 0x00, 0x00 );
+  start[4].set( 0, 100, 0, 0x50c, 3, 0x01, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 );
+  start[5].set( 0,  50, 0, 0x3f2, 8, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xF0, 0x00, 0x00 );
+  start[6].set( 0,  50, 0, 0x3f1, 8, 0xF5, 0x90, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 );
+  start[7].set( 0, 100, 0, 0x50c, 3, 0x01, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 );
 }
 
 void initCycleMessages()
 {
-  cycle[0].set(   0,  500, 0x50c, 3, 0x11, 0x02, 0x00, 0xBE, 0xBE, 0xBE, 0xBE, 0xBE );  
+  cycle[0].set( 0,   0, 500, 0x50c, 3, 0x11, 0x02, 0x00, 0xBE, 0xBE, 0xBE, 0xBE, 0xBE );  
   // 0x3e8: 1st byte 01 or 00, 4th byte 04 or 00; 
   //1st byte 01 - AM 02 - FM1 03 - FM2 04 - PHON 05 - SYNC 06 - DVD 07 - AUX 08 - CD 09 - EMPTY 0A - SAT1 0B - SAT2 0C - SAT3 0D - PHON OE - LINE 0F - 2 clocks
   //3th byte volume
   //4th byte - clock length?
-  cycle[1].set( 400, 1000, 0x3e8, 8, 0x0F, 0x00, 0x29, 0x04, 0x00, 0x00, 0x00, 0x00 );
-  cycle[2].set( 450, 1000, 0x3ef, 8, 0x32, 0x32, 0x32, 0x32, 0x03, 0x00, 0x00, 0x20 );
-  cycle[3].set( 500, 1000, 0x3f2, 8, 0x12, 0x01, 0xFF, 0xFF, 0xFF, 0xF0, 0x00, 0x00 );
+  cycle[1].set( 0, 400, 1000, 0x3e8, 8, 0x0F, 0x00, 0x29, 0x04, 0x00, 0x00, 0x00, 0x00 );
+  cycle[2].set( 0, 450, 1000, 0x3ef, 8, 0x32, 0x32, 0x32, 0x32, 0x03, 0x00, 0x00, 0x20 );
+  cycle[3].set( 0, 500, 1000, 0x3f2, 8, 0x12, 0x01, 0xFF, 0xFF, 0xFF, 0xF0, 0x00, 0x00 );
 }
 
 void initTextMessages()
 {
-  text[ 0].set( 0,   0, 0x336, 8, 0x03, 0x01, 0x0A, 0x01, 0xFE, 0x00, 0x00, 0x00 );
-  text[ 1].set( 0,  50, 0x324, 8, 0x01, 0x01, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00 );
-  text[ 2].set( 0,  50, 0x337, 8, 0x06, 0x20,  ' ',  '@',  ' ',  '@',  ' ', 0x00 );
-  text[ 3].set( 0, 100, 0x336, 8, 0x03, 0x01, 0x05, 0x03, 0x03, 0x00, 0x00, 0x00 );
-  text[ 4].set( 0,  25, 0x324, 8, 0x03, 0x01, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00 );
-  text[ 5].set( 0,  25, 0x337, 8, 0x10, 0x2B,  ' ',  ' ',  ' ',  '@',  ' ',  ' ' );
-  text[ 6].set( 0,  25, 0x337, 8, 0x21,  ' ',  'M',  'e',  'r',  'c',  '@',  'u' );
-  text[ 7].set( 0,  25, 0x337, 8, 0x22,  'r',  'y',  ' ',  ' ',  ' ',  ' ',  ' ' );
-  text[ 8].set( 0,  25, 0x337, 8, 0x23,  ' ',  ' ',  ' ',  ' ',  ' ',  '@',  ' ' );
-  text[ 9].set( 0,  25, 0x337, 8, 0x24,  ' ',  ' ',  'M',  'a',  'r',  'i',  'n' );
-  text[10].set( 0,  25, 0x337, 8, 0x25,  'e',  'r',  ' ',  ' ',  ' ',  ' ',  ' ' );
-  text[11].set( 0,  25, 0x337, 8, 0x26,  ' ',  ' ', 0x00, 0x00, 0x00, 0x00, 0x00 );
+  text[ 0].set( 1000,   0, 500, 0x336, 8, 0x03, 0x01, 0x0A, 0x01, 0xFE, 0x00, 0x00, 0x00 );
+  text[ 1].set( 1000,  50, 500, 0x324, 8, 0x01, 0x01, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00 );
+  text[ 2].set( 1000, 100, 500, 0x337, 8, 0x06, 0x20,  ' ',  '@',  ' ',  '@',  ' ', 0x00 );
+  text[ 3].set( 1000, 200, 500, 0x336, 8, 0x03, 0x01, 0x05, 0x03, 0x03, 0x00, 0x00, 0x00 );
+  text[ 4].set( 1000, 225, 500, 0x324, 8, 0x03, 0x01, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00 );
+  text[ 5].set( 1000, 250, 500, 0x337, 8, 0x10, 0x2B,  ' ',  ' ',  ' ',  '@',  ' ',  ' ' );
+  text[ 6].set( 1000, 275, 500, 0x337, 8, 0x21,  ' ',  'M',  'e',  'r',  'c',  '@',  'u' );
+  text[ 7].set( 1000, 300, 500, 0x337, 8, 0x22,  'r',  'y',  ' ',  ' ',  ' ',  ' ',  ' ' );
+  text[ 8].set( 1000, 325, 500, 0x337, 8, 0x23,  ' ',  ' ',  ' ',  ' ',  ' ',  '@',  ' ' );
+  text[ 9].set( 1000, 350, 500, 0x337, 8, 0x24,  ' ',  ' ',  'M',  'a',  'r',  'i',  'n' );
+  text[10].set( 1000, 375, 500, 0x337, 8, 0x25,  'e',  'r',  ' ',  ' ',  ' ',  ' ',  ' ' );
+  text[11].set( 1000, 400, 500, 0x337, 8, 0x26,  ' ',  ' ', 0x00, 0x00, 0x00, 0x00, 0x00 );
 }
 
 const int SPI_CS_PIN = 10;
@@ -162,6 +167,13 @@ unsigned char rcvLen = 0;
 unsigned long rcvCanId = 0x0;
 unsigned char rcvBuf[8];
 unsigned char sendingNow = 0;
+
+unsigned int sentOnTick = 0;
+
+bool firstCycle = true;
+bool gotClock = false;
+
+int currentText = 0;
 
 MCP_CAN CAN(SPI_CS_PIN); 
 
@@ -197,6 +209,9 @@ void displayText(int strNo, String str)
 {
   byte curLine, curChar, numChars;
 
+  if (DEBUG) {
+    Serial.println(String(strNo) + ": " + str);
+  }
   switch (strNo) {
     case 0:
       curLine = 2;
@@ -286,6 +301,8 @@ void sendStartSequence()
   }
   Serial.println("****");
   timer = 0;
+  firstCycle = true;
+  currentText = 0;
   delay(500);
 }
 
@@ -368,6 +385,7 @@ void loop() {
             temperature = "";
             message = "";
             sendingNow = 0;
+            gotClock = false;
           }
 
           if ((rpm != "0") && (rpm != "") && (sendingNow == 0)) {
@@ -380,25 +398,32 @@ void loop() {
             hour = (((rcvBuf[0] & 0xF8) >> 3) + TZ ) % 24;
             minute = (rcvBuf[1] & 0xFC) >> 2;
             second = (rcvBuf[2] & 0xFC) >> 2;
+            gotClock = true;
             //message=String(hour, DEC) + " " + String(minute, DEC) + " " + String(second, DEC);
         }
           break;
       }
     }
   }
-  
-  if ((sendingNow == 1) || (DEBUG)) {
-    for (int i=0;i<MSG_COUNT;i++) {
-      if ( ((timer % cycle[i].delayed) - cycle[i].started) == 0) {
-        if (cycle[i].header == 0x3f2) {
-          cycle[i].data[0] = decToBcd(hour);
-          cycle[i].data[1] = decToBcd(minute);
-        }
-        CAN.sendMsgBuf(cycle[i].header, 0, cycle[i].len, cycle[i].data);  
-        printDebug(timer, cycle[i]);
-      }
-    }
 
+  if ((sendingNow == 1) || (DEBUG)) {
+    for (int currentCycle = 0; currentCycle < MSG_COUNT; currentCycle ++ ) {
+      if ( ( (timer >= cycle[currentCycle].started ) || (!firstCycle) ) && ((timer % cycle[currentCycle].repeated) - cycle[currentCycle].delayed) == 0) {
+        if (cycle[currentCycle].header == 0x3f2) {
+          cycle[currentCycle].data[0] = decToBcd(hour);
+          cycle[currentCycle].data[1] = decToBcd(minute);
+          if (gotClock) {
+            CAN.sendMsgBuf(cycle[currentCycle].header, 0, cycle[currentCycle].len, cycle[currentCycle].data);
+            sentOnTick = timer;  
+            printDebug(timer, cycle[currentCycle]);
+          }
+        } else {
+          CAN.sendMsgBuf(cycle[currentCycle].header, 0, cycle[currentCycle].len, cycle[currentCycle].data);
+          sentOnTick = timer;  
+          printDebug(timer, cycle[currentCycle]);
+        }
+      }
+    }    
     inSerialData = "";
 
     while (mySerial.available() > 0) {
@@ -411,28 +436,33 @@ void loop() {
           inSerialData = "";
         }
     }
-  
-    if ( (timer % 150) == 0) {
-  
-      // For MQ135
-      /*int sensorValue = analogRead(4);
-      Serial.println(sensorValue);
-      message = String(sensorValue, DEC);*/
-      if (message == "%MTRACK") message = "";
-      
-      displayText(0, padRight(carSpeed,3));
-      displayText(1, padRight(fl, 2) + " RPM:" + padRight(rpm, 4) + " T:" + padRight(temperature, 3) + " " + padRight(fr, 2));
-      displayText(2, padRight(rl, 2) + " " + padCenter(message, TEXT_MSG_LENGTH) + " " + padRight(rr, 2));
-      for (int i=0;i<TEXT_COUNT;i++) {
-        CAN.sendMsgBuf(text[i].header, 0, text[i].len, text[i].data);
-        printDebug(timer, text[i]);
-        delay(text[i].delayed);
+    
+    // For MQ135
+    /*int sensorValue = analogRead(4);
+    Serial.println(sensorValue);
+    message = String(sensorValue, DEC);*/
+    if (message == "%MTRACK") message = "";
+    
+    //Serial.println(">>>>>> " + String(timer) + " " + String(i) + " " + String(timer % text[i].repeated) + " " + String(firstCycle ? text[i].started : 0) + " " + String(text[i].delayed)) + " " + String(((timer % text[i].repeated) - (firstCycle ? text[i].started : 0 ) - text[i].delayed));
+    if ( ( (timer >= text[currentText].started ) || (!firstCycle) ) && ((timer % text[currentText].repeated) - text[currentText].delayed) == 0) {
+      if (currentText == 0) {
+        displayText(0, padRight(carSpeed,3));
+        displayText(1, padRight(fl, 2) + " RPM:" + padRight(rpm, 4) + " T:" + padRight(temperature, 3) + " " + padRight(fr, 2));
+        displayText(2, padRight(rl, 2) + " " + padCenter(message, TEXT_MSG_LENGTH) + " " + padRight(rr, 2));
       }
+      if (sentOnTick == timer) delay(TIMER_STEP/2);
+      CAN.sendMsgBuf(text[currentText].header, 0, text[currentText].len, text[currentText].data);
+      printDebug(timer, text[currentText]);
+      //delay(text[currentText].delayed);
+      currentText = (currentText + 1) % TEXT_COUNT;
     }
-  
+
     delay(TIMER_STEP);
     timer += TIMER_STEP;
-    if (timer >= 32000) timer = 0;
+    if (timer >= 32000) {
+      timer = 0;
+      firstCycle = false;
+    }
   }
 }
 
